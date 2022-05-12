0ebba3e8b4b4de8f2cdbc8f42e31cd2b97805c26
TEERAWAT PRAROM <teewat@yahoo.com>
2022-05-12 19:36:09 +1000

    fixed Gemfile.lock platform

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:teewat888/depot2.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
+set :branch, 'main'
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
 set :deploy_to, "/home/deploy/#{fetch :application}"
