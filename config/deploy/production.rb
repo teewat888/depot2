8ea9be13aa673b9169cc2dc5238b91b5012562f0
TEERAWAT PRAROM <teewat@yahoo.com>
2022-05-12 21:48:10 +1000

    added: production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '192.168.86.38', user: 'deploy', roles: %w{app db web}
+server '3.106.137.181', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
